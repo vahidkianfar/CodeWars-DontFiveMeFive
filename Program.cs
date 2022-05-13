0806622c95e5c6b114088a2169ebcafa765e2f6d
vahidkianfar <vahidkianfar@gmail.com>
2022-05-13 17:15:20 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Linq;
+
+int start = 4;
+int end = 17;
+string five= "5";
+
+int distance = end - start;
+foreach (var i in Enumerable.Range(start, ++distance).Where(x=> !x.ToString().Contains(five)))
+{
+    Console.Write(i+" ");
+}
